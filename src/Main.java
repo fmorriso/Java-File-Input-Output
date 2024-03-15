4b5403ee617626e3ea6f99ddfd08431e13667e99
Frederick Morrison <frederick.morrison@outlook.com>
2024-03-14 22:04:02 -0600

    simplify imports

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-/*import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;*/
 import java.io.*;
 
 public class Main
